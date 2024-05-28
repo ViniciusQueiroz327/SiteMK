98e14c97438549288d1891e27583083def029aec
ViniciusQueiroz327 <gm.gb@outlook.com>
2024-05-28 14:48:57 -0300

    VERSÃO CRIADA

---
+++ HTML5/assets/script.js
@@ -0,0 +1,9 @@
+function openLoginPopup() {
+    document.getElementById("loginPopup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeLoginPopup() {
+    document.getElementById("loginPopup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
